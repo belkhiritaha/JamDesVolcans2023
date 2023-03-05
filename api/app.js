31ca7d10a4901dc218459a2b2fe3cf34c0a6d20f
Paul <paul@Unknown.com>
2023-03-05 10:19:22 +0100

    deck modif

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const clickTypes = {
     0: [{x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}, {x: 4, y: 1}],
     1: [{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 1, y: 4}]
 };
-
+ 
 const deckCards = {
     0: {imageSrc: "http://jamdesvolcans:3000/cartes/construction/", title: "Construction 1 ", description: "", cost: "10" },
     1: {imageSrc: "http://jamdesvolcans:3000/cartes/construction/", title: "Construction 2 ", description: "", cost: "10" },
